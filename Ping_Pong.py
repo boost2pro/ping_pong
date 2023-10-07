376a10291130d58220d268b0962c8f4b48fb12d7
boost2pro <146555124+boost2pro@users.noreply.github.com>
2023-10-07 16:30:48 +0300

    Add files via upload
    add pictures and update file

--- Ping_Pong.py
+++ Ping_Pong.py
@@ -4,7 +4,7 @@ from pygame import *
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (wight, hieght))
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -18,16 +18,16 @@ class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
-            self.rect.x -= self.speed
+            self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
-            self.rect.x += self.speed
+            self.rect.y += self.speed
     
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
-            self.rect.x -= self.speed
+            self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < win_height - 80:
-            self.rect.x += self.speed
+            self.rect.y += self.speed
 
 back = (200, 255, 255)
 win_width = 600
@@ -35,18 +35,33 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+speed_x = 3
+speed_y = 3
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-        if finish != True:
-            window.fill(back)
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-        display.update()
-        clock.tick(FPS)
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
